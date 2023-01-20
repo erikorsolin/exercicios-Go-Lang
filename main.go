2081ae0bb788a68066b672245d12d0605d7c83d5
Erik Orsolin <depaulaerik670@gmail.com>
2023-01-19 23:11:20 -0300

    exercicio1

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+type meu_tipo int
+
+var b meu_tipo
+
+func main() {
+	n1 := 10
+	n2 := 20
+	fmt.Println("rodando")
+	fmt.Printf("%T\n", b)
+	soma(n1, n2)
+	multiplica(n1, n2)
+	imprime_text()
+
+}
+
+func soma(x int, y int) {
+
+	fmt.Println("a soma é:", x+y)
+
+}
+
+func multiplica(x int, y int) {
+	fmt.Println("a multiplicação é:", x*y)
+
+}
+
+func imprime_text() {
+	x := "abc "
+	y := "def "
+	z := "ghi"
+	w := fmt.Sprint(x, y, z)
+	fmt.Println(w)
+}
