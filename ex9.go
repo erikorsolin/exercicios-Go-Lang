a80698400f088bc1ae891cbd3868c016cb4d455e
Erik Orsolin <depaulaerik670@gmail.com>
2023-02-05 18:12:33 -0300

    exercicios

---
+++ ex9.go
@@ -0,0 +1,23 @@
+package main
+
+import 	"fmt"
+
+var b int
+var y int
+var z int
+var w int
+
+func main(){
+
+	b = 10
+	z = 20
+	y = 20
+	w = 9
+	fmt.Println(b < y)
+	fmt.Println(z > w)
+	fmt.Println(b != w)
+	fmt.Println(w == z)
+	
+
+}
+
